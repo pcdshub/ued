a0b0818f5409b68c6fa1c68a667baf876c4bdd12
Ken Lauer <klauer@slac.stanford.edu>
2021-05-19 10:19:05 -0700

    MNT: fix ui_objs path

--- queueserver/ioc-ued-bsgui-qs/ipython_test.sh
+++ queueserver/ioc-ued-bsgui-qs/ipython_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # ipython check of what we would load for queue server
 source /cds/group/pcds/pyps/apps/hutch-python/ued/uedenv
-ipython -i /cds/group/pcds/pyps/apps/hutch-python/ued/queueserver/ioc-ued-bsgui/ui_objs.py
+ipython -i /cds/group/pcds/pyps/apps/hutch-python/ued/queueserver/ioc-ued-bsgui-qs/ui_objs.py
