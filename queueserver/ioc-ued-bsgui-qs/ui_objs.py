f78a8dea5fa54fc7021c9f31cae6734c08b36ca0
Zachary Lentz <zlentz@slac.stanford.edu>
2022-02-28 16:13:19 -0800

    MAINT: restore hard-coded path

--- queueserver/ioc-ued-bsgui-qs/ui_objs.py
+++ queueserver/ioc-ued-bsgui-qs/ui_objs.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from hutch_python.load_conf import load
 
 
-objs = load('/cds/home/z/zlentz/github/ued/conf.yml')
+objs = load('/cds/group/pcds/pyps/apps/hutch-python/ued/conf.yml')
 globals().update(**objs)
